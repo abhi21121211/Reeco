aa499fba53abd03cb3b2712f3e932705afbfd9f0
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2023-12-15 10:39:43 +0530

    missing button done

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,43 +1,39 @@
-
+// Products.jsx
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { fetchProducts } from '../store/productSlice';
-import Apple from '../images/Apple.png';
-import Avocado from '../images/Avocado.png';
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import CloseIcon from '@mui/icons-material/Close';
+import DoneIcon from '@mui/icons-material/Done';
 
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { fetchProducts, approveProduct, markProductMissing } from "../store/productSlice";
+import Avocado from "../images/Avocado.png";
 
 function Products() {
   const dispatch = useDispatch();
   const { products, loading, error } = useSelector((state) => state.products);
-  console.log(products)
+
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
+
+  const handleApproval = (productId) => {
+    dispatch(approveProduct(productId));
+  };
+
+  const handleRejection = (productId) => {
+    dispatch(markProductMissing({ productId, urgent: true }));
+  };
 
   if (loading) {
     return <p>Loading...</p>;
@@ -48,13 +44,15 @@ function Products() {
   }
 
   return (
-    <div style={{ width: '90%', background: "white", border: "1px solid black", borderRadius: '10px', margin: 'auto', marginTop: '20px' }}>
-      <div style={{ margin: '20px' }}>
-        <div style={{ display: "flex", justifyContent: 'space-between', marginBottom:"20px"}}>
-          <input type="text" placeholder='Search....' style={{ width: '40%', borderRadius: '20px', border: '1px solid black', }} />
+    <div style={{ width: "90%", background: "white", border: "1px solid black", borderRadius: "10px", margin: "auto", marginTop: "20px" }}>
+      <div style={{ margin: "20px" }}>
+        <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "20px" }}>
+          <input type="text" placeholder="Search...." style={{ width: "40%", borderRadius: "20px", border: "1px solid black" }} />
           <div>
-            <button style={{ border: "1px solid #1e633f", fontSize: '15px', borderRadius: "50px", padding: '8px', width: '100px', background: "white", color: '#1e633f', fontWeight: 'bold' }}>Add item</button>
-            <i className='pi pi-print' style={{ fontSize: "20px", paddingLeft: '10px', color: '#1e633f' }}></i>
+            <button style={{ border: "1px solid #1e633f", fontSize: "15px", borderRadius: "50px", padding: "8px", width: "100px", background: "white", color: "#1e633f", fontWeight: "bold" }}>
+              Add item
+            </button>
+            <i className="pi pi-print" style={{ fontSize: "20px", paddingLeft: "10px", color: "#1e633f" }}></i>
           </div>
         </div>
         <div>
@@ -63,49 +61,66 @@ function Products() {
               <TableHead>
                 <TableRow>
                   <TableCell>Image</TableCell>
-                  <TableCell size="10px" component="th" scope="row">Product name</TableCell>
+                  <TableCell component="th" scope="row">
+                    Product name
+                  </TableCell>
                   <TableCell align="left">Brand</TableCell>
-                  <TableCell >Price</TableCell>
-                  <TableCell >Quantity</TableCell>
-                  <TableCell >Total</TableCell>
-                  <TableCell  > Status</TableCell>
-                  <TableCell > </TableCell>
-                  <TableCell > </TableCell>
-                  <TableCell > </TableCell>
-                  <TableCell > </TableCell>
-                  <TableCell > </TableCell>
-                  <TableCell > </TableCell>
+                  <TableCell>Price</TableCell>
+                  <TableCell>Quantity</TableCell>
+                  <TableCell>Total</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-
                 {products.map((pro) => (
                   <TableRow key={pro.id}>
                     <TableCell component="th" scope="row">
-                      <img src={Avocado} style={{ width: '50px' }} />
+                      <img src={Avocado} style={{ width: "50px" }} alt={pro.title} />
                     </TableCell>
                     <TableCell>{pro.title}</TableCell>
-                    <TableCell align="">{pro.brand}</TableCell>
-                    <TableCell align="">{pro.price}</TableCell>
-                    <TableCell align="">{pro.quantity}</TableCell>
-                    <TableCell align="">{pro.total}</TableCell>
-                    <TableCell align="" style={{ background: "#f3f3f3" }}>{pro.status}</TableCell>
-                    <TableCell colSpan={7} align="right" style={{ background: "#f3f3f3" }}> <i className="pi pi-check" style={{ fontSize: '1.5rem', margin: '10px' }}></i>
-                      <i className="pi pi-times" style={{ fontSize: '1.5rem', margin: '10px' }}></i>
-                      <button style={{ fontSize: '1.5rem', marginLeft: '5px', marginTop: "-50px", border: 'none' }}>Edit</button>
+                    <TableCell>{pro.brand}</TableCell>
+                    <TableCell>{pro.price}</TableCell>
+                    <TableCell>{pro.quantity}</TableCell>
+                    <TableCell>{pro.total}</TableCell>
 
+                    <TableCell style={{ background: "#f3f3f3" }}>
+                      {pro.status === "Missing - Urgent" ? (
+                        <button style={{ border: "1px solid #1e633f", fontSize: '15px', borderRadius: "50px", padding: '8px', width: '150px', background: "red", color: 'white', fontWeight: 'bold', marginLeft: '20px' }}>{pro.status}</button>
+                      ) : pro.status === "Missing" ? (
+                        <button style={{ border: "1px solid #1e633f", fontSize: '15px', borderRadius: "50px", padding: '8px', width: '150px', background: "#f66d44", color: 'white', fontWeight: 'bold', marginLeft: '20px' }}>{pro.status}</button>
+                      ) : pro.status === "Approved" ? (
+                        <button style={{ border: "1px solid #1e633f", fontSize: '15px', borderRadius: "50px", padding: '8px', width: '150px', background: "green", color: 'white', fontWeight: 'bold', marginLeft: '20px' }}>{pro.status}</button>
+                      ) : (
+                        <button style={{ display: "none" }}></button>
+                      )}
                     </TableCell>
 
+
+
+                    <TableCell colSpan={7} align="right" style={{ background: "#f3f3f3" }}>
+                      <IconButton onClick={() => handleApproval(pro.id)}>
+                        <DoneIcon style={{ fontSize: "1.5rem", margin: "10px" }} />
+                      </IconButton>
+                      <IconButton onClick={() => handleRejection(pro.id)}>
+                        <CloseIcon style={{ fontSize: "1.5rem", margin: "10px" }} />
+                      </IconButton>
+                      <button style={{ fontSize: "1.5rem", marginLeft: "5px", marginTop: "-50px", border: "none" }}>Edit</button>
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </TableContainer>
-
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,31 +1,51 @@
 // productSlice.js
-
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchProducts = createAsyncThunk("product/fetchProducts", async () => {
   const response = await fetch("http://localhost:3000/products");
- 
-   
   const data = await response.json();
-  console.log(data);
   return data;
+});
+
+// Async thunk for marking a product as approved
+export const approveProduct = createAsyncThunk("product/approveProduct", async (productId) => {
+  // Perform an API call to update the status on the server
+  const response = await fetch(`http://localhost:3000/products/${productId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ status: "Approved" }),
+  });
+  const updatedProduct = await response.json();
+  return updatedProduct;
+});
+
+// Async thunk for marking a product as missing
+export const markProductMissing = createAsyncThunk("product/markProductMissing", async ({ productId, urgent }) => {
+  // Perform an API call to update the status on the server
+  const status = urgent ? "Missing - Urgent" : "Missing";
+  const response = await fetch(`http://localhost:3000/products/${productId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ status }),
+  });
+  const updatedProduct = await response.json();
+  return updatedProduct;
 });
 
 const productSlice = createSlice({
   name: "product",
   initialState: {
-    products: [], // Ensure products is initialized as an array
+    products: [],
     loading: false,
     error: null,
   },
   reducers: {
+    // Your existing reducers here...
     showProducts(state, action) {
-      // Modify state as needed
-    },
-    productMissing(state, action) {
-      // Modify state as needed
-    },
-    productApproved(state, action) {
       // Modify state as needed
     },
     productUpdate(state, action) {
@@ -41,15 +61,30 @@ const productSlice = createSlice({
         state.loading = false;
         state.products = action.payload;
         state.error = null;
-        console.log(state.products);
       })
       .addCase(fetchProducts.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
+      })
+      // Handle the approval action
+      .addCase(approveProduct.fulfilled, (state, action) => {
+        const updatedProduct = action.payload;
+        const index = state.products.findIndex((pro) => pro.id === updatedProduct.id);
+        if (index !== -1) {
+          state.products[index] = updatedProduct;
+        }
+      })
+      // Handle the mark missing action
+      .addCase(markProductMissing.fulfilled, (state, action) => {
+        const updatedProduct = action.payload;
+        const index = state.products.findIndex((pro) => pro.id === updatedProduct.id);
+        if (index !== -1) {
+          state.products[index] = updatedProduct;
+        }
       });
   },
 });
 
-export const { showProducts, productMissing, productApproved, productUpdate } = productSlice.actions;
+export const { showProducts, productUpdate } = productSlice.actions;
 
 export default productSlice.reducer;
