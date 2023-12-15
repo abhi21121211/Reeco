4abcc7e285a32b26ecd3d241ff9eabd6822d6051
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2023-12-15 11:37:22 +0530

    missing and approved button done

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,5 +1,4 @@
-// Products.jsx
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Box from "@mui/material/Box";
 import Collapse from "@mui/material/Collapse";
@@ -11,9 +10,13 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import CloseIcon from '@mui/icons-material/Close';
-import DoneIcon from '@mui/icons-material/Done';
-
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import CloseIcon from "@mui/icons-material/Close";
+import DoneIcon from "@mui/icons-material/Done";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { fetchProducts, approveProduct, markProductMissing } from "../store/productSlice";
@@ -22,6 +25,9 @@ import Avocado from "../images/Avocado.png";
 function Products() {
   const dispatch = useDispatch();
   const { products, loading, error } = useSelector((state) => state.products);
+  const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
+  const [selectedProductId, setSelectedProductId] = useState(null);
+  const [urgentMark, setUrgentMark] = useState(false);
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -31,8 +37,19 @@ function Products() {
     dispatch(approveProduct(productId));
   };
 
-  const handleRejection = (productId) => {
-    dispatch(markProductMissing({ productId, urgent: true }));
+  const handleMarkMissing = (productId, urgent) => {
+    setSelectedProductId(productId);
+    setUrgentMark(urgent);
+    setConfirmationDialogOpen(true);
+  };
+
+  const handleConfirmationDialogClose = () => {
+    setConfirmationDialogOpen(false);
+  };
+
+  const handleConfirmationDialogConfirm = () => {
+    dispatch(markProductMissing({ productId: selectedProductId, urgent: urgentMark }));
+    setConfirmationDialogOpen(false);
   };
 
   if (loading) {
@@ -89,7 +106,7 @@ function Products() {
                     <TableCell>{pro.quantity}</TableCell>
                     <TableCell>{pro.total}</TableCell>
 
-                    <TableCell style={{ background: "#f3f3f3" }}>
+                    {/* <TableCell style={{ background: "#f3f3f3" }}>
                       {pro.status === "Missing - Urgent" ? (
                         <button style={{ border: "1px solid #1e633f", fontSize: '15px', borderRadius: "50px", padding: '8px', width: '150px', background: "red", color: 'white', fontWeight: 'bold', marginLeft: '20px' }}>{pro.status}</button>
                       ) : pro.status === "Missing" ? (
@@ -99,17 +116,67 @@ function Products() {
                       ) : (
                         <button style={{ display: "none" }}></button>
                       )}
+                    </TableCell> */}
+
+                    <TableCell style={{ background: "#f3f3f3" }}>
+                      {pro.status === "Missing - Urgent" || pro.status === "Missing" ? (
+                        <button
+                          style={{
+                            border: "1px solid #1e633f",
+                            fontSize: "15px",
+                            borderRadius: "50px",
+                            padding: "8px",
+                            width: "150px",
+                            background: pro.status === "Missing - Urgent" ? "red" : "#f66d44",
+                            color: "white",
+                            fontWeight: "bold",
+                            marginLeft: "20px",
+                          }}
+                          onClick={() => handleMarkMissing(pro.id, !pro.status === "Missing - Urgent")}
+                        >
+                          {pro.status}
+                        </button>
+                      ) : pro.status === "Approved" ? (
+                        <button
+                          style={{
+                            border: "1px solid #1e633f",
+                            fontSize: "15px",
+                            borderRadius: "50px",
+                            padding: "8px",
+                            width: "150px",
+                            background: "green",
+                            color: "white",
+                            fontWeight: "bold",
+                            marginLeft: "20px",
+                          }}
+                        >
+                          {pro.status}
+                        </button>
+                      ) : (
+                        <button style={{ display: "none" }}></button>
+                      )}
                     </TableCell>
-
-
 
                     <TableCell colSpan={7} align="right" style={{ background: "#f3f3f3" }}>
                       <IconButton onClick={() => handleApproval(pro.id)}>
-                        <DoneIcon style={{ fontSize: "1.5rem", margin: "10px" }} />
+                        {pro.status === "Approved" ? (
+                          <DoneIcon style={{ fontSize: "1.5rem", margin: "10px", color: "green" }} />
+                        ) : (
+                          <DoneIcon style={{ fontSize: "1.5rem", margin: "10px" }} />
+                        )}
                       </IconButton>
-                      <IconButton onClick={() => handleRejection(pro.id)}>
+
+
+                      <IconButton onClick={() => handleMarkMissing(pro.id, true)}>
+                        {pro.status === "Missing - Urgent" ? (
+                          <CloseIcon style={{ fontSize: "1.5rem", margin: "10px", color: "red" }} />
+                        ) : pro.status === "Missing" ? (
+                          <CloseIcon style={{ color: "#f66d44", fontSize: "1.5rem", margin: "10px" }} />
+                        ) : (<CloseIcon style={{ fontSize: "1.5rem", margin: "10px" }} />)}
+                      </IconButton>
+                      {/* <IconButton onClick={() => handleMarkMissing(pro.id, false)}>
                         <CloseIcon style={{ fontSize: "1.5rem", margin: "10px" }} />
-                      </IconButton>
+                      </IconButton> */}
                       <button style={{ fontSize: "1.5rem", marginLeft: "5px", marginTop: "-50px", border: "none" }}>Edit</button>
                     </TableCell>
                   </TableRow>
@@ -119,6 +186,18 @@ function Products() {
           </TableContainer>
         </div>
       </div>
+
+      {/* Confirmation Dialog */}
+      <Dialog open={confirmationDialogOpen} onClose={handleConfirmationDialogClose}>
+        <DialogTitle>Confirmation</DialogTitle>
+        <DialogContent>
+          Do you want to mark the product as {urgentMark ? "Missing - Urgent" : "Missing"}?
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleConfirmationDialogClose}>No</Button>
+          <Button onClick={handleConfirmationDialogConfirm}>Yes</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
